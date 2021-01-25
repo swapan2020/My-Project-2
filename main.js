3e463e405d84e4426562860d954ec8174dfbef0e
swapan2020 <73292773+swapan2020@users.noreply.github.com>
2021-01-26 00:37:19 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+ 
+
+window.addEventListener('scroll', function(){
+	var s = document.querySelector('header');
+	s.classList.toggle('sticky',window.scrollY > 0);
+})
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
